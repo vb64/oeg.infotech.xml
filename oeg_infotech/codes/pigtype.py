1405259ec4e8ee2c2b2b0c0788bfccde1cd867b4
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2019-03-15 23:47:46 +0400

    split codes file to separate modules

---
+++ oeg_infotech/codes/pigtype.py
@@ -0,0 +1,20 @@
+"""
+Inspection tool types
+"""
+
+
+class PigType:  # pylint: disable=old-style-class,no-init,too-few-public-methods
+    """
+    codes for pig tools
+    """
+    MFL = '990005096296'
+    TFI = '990004033563'
+    CALIPER_MECH = '990004033561'
+    NAVIGATE = '6907295'
+
+    ULTRASONIC = '990004033557'
+    CALIPER_MAGN = '990004033558'
+    EMAP = '6907298'
+    COMBO = '6915601'
+    CLEANER = '990004033560'
+    CALIPER_TOOL = '990004033562'
