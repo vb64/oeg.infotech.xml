4fe7b82ae413595337b1e9e4e7912438b24032b0
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2020-02-08 13:35:23 +0400

    Move bages to Codacy (#12)

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ Library for OrgEnergoGaz Infotech xml processing
 """
 
 setuptools.setup(
-    name = 'oeg_infotech',
-    version = '1.4.1',
-    author = 'Vitaly Bogomolov',
-    author_email = 'mail@vitaly-bogomolov.ru',
-    description = 'Library for OrgEnergoGaz Infotech xml processing',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url = 'https://github.com/vb64/oeg.infotech.xml',
-    packages = ['oeg_infotech', 'oeg_infotech.codes'],
-    download_url = 'https://github.com/vb64/oeg.infotech.xml/archive/v1.4.1.tar.gz',
-    keywords = ['python', 'OrgEnergoGaz', 'Infotech', 'IUST', 'xml'],
-    classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+  name='oeg_infotech',
+  version='1.4.1',
+  author='Vitaly Bogomolov',
+  author_email='mail@vitaly-bogomolov.ru',
+  description='Library for OrgEnergoGaz Infotech xml processing',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/vb64/oeg.infotech.xml',
+  packages=['oeg_infotech', 'oeg_infotech.codes'],
+  download_url='https://github.com/vb64/oeg.infotech.xml/archive/v1.4.1.tar.gz',
+  keywords=['python', 'OrgEnergoGaz', 'Infotech', 'IUST', 'xml'],
+  classifiers=[
+    "Programming Language :: Python :: 2.7",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
 )
